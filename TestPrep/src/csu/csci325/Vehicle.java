1e033087d1c61a182a820ec9c9f4f3af078deb79
Natemixon7 <nrmixon@csustudent.net>
2021-02-26 22:03:24 -0500

    Vehicle Class

---
+++ TestPrep/src/csu/csci325/Vehicle.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package csu.csci325;
+
+/**
+ *
+ * @author natem
+ */
+
+public class Vehicle 
+{
+    private int speed;
+    
+    public Vehicle ()
+    {
+        speed = 0;
+        
+    }
+    public Vehicle (int vehicleSpeed)
+    {
+        speed = vehicleSpeed;
+        
+    }
+    public int GetSpeed()
+    {
+        return speed;
+    }
+    public void SetSpeed(int newSpeed)
+    {
+        speed = newSpeed;
+    }
+    public void PrintVehicle()
+    {
+        System.out.println("Vehicle speed: " + speed);
+    }
+}
